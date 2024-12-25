63f0f48553486d5ec77f01843fdfd43f022b22f6
Atlas-Lofton <gll4392@Hotmail.com>
2024-12-25 15:12:09 -0600

    added some arithmetic operators.

--- js/index.js
+++ js/index.js
@@ -2,3 +2,24 @@
 // that are not declared
 "use strict"
 
+let books = 5;
+
+console.log(books);
+
+
+    books =  books + 1;
+
+console.log(books);
+
+books = books - 3;
+
+console.log(books);
+
+books = books *4;
+
+console.log(books);
+
+books = books / 4;
+
+console.log(books);
+
