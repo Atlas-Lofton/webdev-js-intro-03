c0c2f40d6b8901570343c72d8c98fe6a456897ce
Atlas-Lofton <gll4392@Hotmail.com>
2025-01-01 17:14:47 -0600

    Expiremented with various types of operators. Forgot to push until now.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 // that are not declared
 "use strict"
 
-let books = 5;
+/* let books = 5;
 
 console.log(books);
 
@@ -23,3 +23,33 @@ books = books / 4;
 
 console.log(books);
 
+
+books += 1;
+console.log(books);
+books += 1;
+console.log(books);
+
+books -= 2;
+console.log(books);
+books %= 3;
+console.log(books);
+
+books = 5;
+
+books != 5;
+console.log(books);
+
+if (books > 5) {
+    console.log("You need to go shopping!");
+} else 
+    {
+        console.log("You have just enough.");
+    }
+
+    if (books !=5) {
+        console.log("You do not have five books.");
+    } else {
+        console.log("Look at that! Five books.");
+    }
+
+*/
